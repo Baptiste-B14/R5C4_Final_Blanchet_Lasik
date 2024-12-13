5c584ab6bd44871f9a12e4a82f887c1e28a5b7f4
Baptiste-B14 <blanchet.baptiste77@gmail.com>
2024-12-13 13:51:19 +0100

    init angular

--- frontend/README.md
+++ frontend/README.md
@@ -1,4 +1,4 @@
-# TpNote
+# Frontend
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.8.
 
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,14 +1,3 @@
 import { Routes } from '@angular/router';
-import { HomepageComponent } from './homepage/homepage.component';
-import { ChartPageComponent } from './chart-page/chart-page.component';
 
-export const routes: Routes = [
-  {
-    path: '',
-    component: HomepageComponent,
-  },
-  {
-    path: 'dashboard',
-    component: ChartPageComponent,
-  },
-];
+export const routes: Routes = [];
