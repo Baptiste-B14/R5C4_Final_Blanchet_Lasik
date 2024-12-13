1eacd1ae21abf80a8d344fff187888d4fd34efff
Baptiste-B14 <blanchet.baptiste77@gmail.com>
2024-12-13 14:56:10 +0100

    start of front

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {HomepageComponent} from "./homepage/homepage.component";
+import {VisualisationComponent} from "./visualisation/visualisation.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent,
+  },
+
+  {
+    path: 'visualisation',
+    component: VisualisationComponent,
+  },
+];
