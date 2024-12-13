18ba8b91dee69342140b667c8976e05516177c05
Baptiste-B14 <blanchet.baptiste77@gmail.com>
2024-12-13 16:06:43 +0100

    end of visualisation

--- frontend/src/app/visu/visu.component.ts
+++ frontend/src/app/visu/visu.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import { Search} from "../models/Search";
 
 @Component({
   selector: 'app-visu',
@@ -9,14 +10,42 @@ import {HttpClient} from "@angular/common/http";
   styleUrl: './visu.component.css'
 })
 export class VisuComponent implements OnInit {
-  searches: [] = [];
+  searches: Search[] = [];
+  currentPage: number = 1;
+  itemsPerPage: number = 30;
+  paginatedSearches: Search[] = [];
 
   constructor(private readonly http: HttpClient) {
   }
 
   ngOnInit() {
-    this.http.get<any>('http://localhost:5000/searches').subscribe((data) => (this.searches = data))
+    this.http.get<Search[]>('http://localhost:5000/searches?page=1&limit=1000')
+      .subscribe((data) => {
+        this.searches = data;
+        this.updatePage();
+      });
+  }
+
+  updatePage() {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.paginatedSearches = this.searches.slice(startIndex, endIndex);
+
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updatePage();
+    }
   }
 
 
+  nextPage() {
+    if (this.currentPage * this.itemsPerPage < this.searches.length) {
+      this.currentPage++;
+      this.updatePage();
+    }
+  }
 }
+
